6ab70d1513ed50d81bd3f3d7b95b4bca71a33648
Ryanmufasa <97072139+Ryanmufasa@users.noreply.github.com>
2022-06-10 09:32:18 +0900

    ignore 추가

--- src/com/distributor/AP/DistributorImpl.java
+++ src/com/distributor/AP/DistributorImpl.java
@@ -6,4 +6,5 @@ import javax.servlet.http.HttpServletResponse;
 public interface DistributorImpl {
 	
 	public void distributor(HttpServletRequest request, HttpServletResponse response) throws Exception;
+	
 }
