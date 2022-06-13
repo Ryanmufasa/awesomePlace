4860caf1abdb4b3beb05d0d874699febafb4fe60
Yoooong <92244572+YoooonJJ@users.noreply.github.com>
2022-06-13 10:46:45 +0900

    컨트롤러 작성 예시 파일

---
+++ src/com/frontcontroller/AP/APController.java
@@ -0,0 +1,77 @@
+// 참고용
+package com.frontcontroller.AP;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.NextPage;
+import service.ServiceInterface;
+
+@WebServlet("*.do")
+public class APController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public APController() {
+        super();
+    }
+
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	
+    	request.setCharacterEncoding("UTF-8");
+    	response.setCharacterEncoding("UTF-8");
+    	
+    	ServiceInterface serv = null;
+    	NextPage page = null;
+    	
+    	String requestURL = request.getRequestURL().toString();
+    	System.out.println(requestURL);
+    	
+    	String requestPath = request.getContextPath().toString();
+    	System.out.println(requestPath);
+    	
+    	int start = requestURL.lastIndexOf("/");
+    	
+    	String path = requestURL.substring(start);
+    	System.out.println("path : " + path);
+    	
+
+    	String path2 = request.getRequestURL().substring(request.getContextPath().length());
+    	System.out.println("path2 : " + path2);
+
+    	    	
+    	switch(path) {
+    	case "/joinForm.do":
+    		page = new NextPage("/awesomePlace/join/joinForm.jsp", true);
+    		break;
+		/*
+		 * case "/join.do" : serv = new JoinService(); page = new NextPage("#", true);
+		 */
+    	}
+    	
+    	if(serv != null) {
+    		serv.execute(request, response);
+    	}
+    	
+    	if(page.isRedirect()) {
+    		response.sendRedirect(page.getNextPath());
+    	}else {
+    		request.getRequestDispatcher(page.getNextPath()).forward(request, response);
+    	}
+
+    	
+    	
+    	
+    	
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		doGet(request, response);
+	}
+
+}
